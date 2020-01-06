6a701a82ae2a5f68992c5ae4dbcc036f5bb2fca7
mariobalrod <marioballestero.rdg@gmail.com>
2020-01-06 21:19:43 +0100

    Some changes of the App component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import Navbar from '../components/Navbar'
+import Searchbar from '../components/Searchbar'
+import Content from '../components/Content'
+
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      movies: [],
+      searchTerm: ''
+    }
+    this.apiKey = process.env.REACT_APP_API
+  }
+
+  /* Funcion para cuando se le da enter en el buscador con un buscador */
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}`)
+    .then(data => data.json())
+    .then(data => {
+      console.log(data);
+      this.setState({ movies: [...data.results]})
+    })
+  }
+
+  /* Funcion para cuando se cambia el valor de la busqueda */
+  handleChange = (e) => {
+    this.setState({ searchTerm: e.target.value})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Navbar />
+        <Searchbar  handleSubmit={this.handleSubmit} handleChange={this.handleChange} />
+        <Content movies={this.state.movies} />
+        
+      </div>
+    );
+  }
 }
 
 export default App;
